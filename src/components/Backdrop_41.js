2a32bb3af3a8b623f468c5a396bb79f809a29cfe
unknown <a29692130@gmail.com>
2021-10-07 19:58:31 +0800

    add <Modal />, <Backdrop /> components

---
+++ src/components/Backdrop_41.js
@@ -0,0 +1,5 @@
+function Backdrop_41(props) {
+  return <div className='backdrop' onClick={props.onClose}></div>;
+}
+
+export default Backdrop_41;
--- src/components/Todo_41.js
+++ src/components/Todo_41.js
@@ -1,14 +1,33 @@
-function Todo_41(props){
-    return (
-        <div>
-          <div className="card">
-            <h2>{props.text}</h2>
-            <div className="actions">
-              <button className="btn">Delete</button>
-            </div>
-          </div>
+import { useState } from 'react';
+import Backdrop_41 from '../components/Backdrop_41';
+import Modal_41 from '../components/Modal_41';
+
+function Todo_41(props) {
+  const [showModal, setShowModal] = useState();
+
+  function showModalHandler() {
+    setShowModal(true);
+  }
+  function closeModalHandler() {
+    setShowModal(false);
+  }
+
+  return (
+    <div>
+      <div className='card'>
+        <h2>{props.text}</h2>
+        <div className='actions'>
+          <button className='btn' onClick={showModalHandler}>
+            Delete
+          </button>
         </div>
-    );
+      </div>
+      {showModal && <Backdrop_41 onClose={closeModalHandler} />}
+      {showModal && (
+        <Modal_41 text='Are you sure?' onClose={closeModalHandler} />
+      )}
+    </div>
+  );
 }
 
 export default Todo_41;
